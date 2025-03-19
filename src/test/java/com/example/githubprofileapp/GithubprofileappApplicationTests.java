b3f7a95f9a2fc81eed67fef1fe3e8a7aa130e063
“Patrick <patrickfloriansoftwareengineer@gmail.com>
2025-03-18 19:03:51 -0500

    Initial commit

---
+++ src/test/java/com/example/githubprofileapp/GithubprofileappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubprofileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubprofileappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
